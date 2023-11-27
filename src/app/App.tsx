3e7fd3942e46d144cd4ccc417787ff1f76d77854
Anton Soldatov <turbolet85@gmail.com>
2023-11-27 22:25:14 +0100

    Start setup routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { About, HomeLayout } from '../pages';
+import { About, Cocktail, HomeLayout } from '../pages';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +10,10 @@ const router = createBrowserRouter([
   {
     path: '/about',
     element: <About />,
+  },
+  {
+    path: '/cocktail',
+    element: <Cocktail />,
   },
 ]);
 
