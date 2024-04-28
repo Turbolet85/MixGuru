ea6488038364d7c4be3e8baf1cbfbfdff23243fa
Anton Soldatov <turbolet85@gmail.com>
2024-04-28 21:48:23 +0200

    add single cocktail

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,19 @@
 import '../styles/variables.css';
 import '../styles/global.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-  <App />,
+  <>
+    <ToastContainer position={'top-center'} autoClose={2000} />,
+    <App />,
+  </>,
+
   // </React.StrictMode>,
 );
--- src/pages/Newsletter.tsx
+++ src/pages/Newsletter.tsx
@@ -1,5 +1,21 @@
 const Newsletter = () => {
-  return <h2>Newsletter</h2>;
+  return (
+    <form className={'form'}>
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>our Newsletter</h4>
+      <div className="form-row">
+        <label htmlFor={'name'} className={'form-label'}>
+          name
+        </label>
+        <input
+          type={'text'}
+          className={'form-input'}
+          name={'name'}
+          id={'name'}
+          defaultValue={'Anton'}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default Newsletter;
